2de7ca6b153894881c8ef60fef9c8f43c9cec45f
Your Name <you@example.com>
2017-12-23 17:50:05 +0300

    -

--- ArmoredMonster.py
+++ ArmoredMonster.py
@@ -2,6 +2,11 @@ class ArmoredMonster(BaseMonster):
     def __init__(self, hp, damage,armor):
         super(ArmoredMonster, self).__init__(hp, damage)
         self._armor = armor
+        self.__private_var=10
+    def _protected_method(self):
+        pass
+    def __private_method(self):
+        pass
     def take_damage(self,amount):
         if self._armor>=amount:
             self._armor-=amount
